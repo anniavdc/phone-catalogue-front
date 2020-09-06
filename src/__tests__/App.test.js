0528f72e5f72dd5cec1f5d12a1afbd2291bbce75
annia <avdacosta@paradigmadigital.com>
2020-09-06 22:30:23 +0200

    update app test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import { render } from '@testing-library/react';
+
+// files
 import App from '../App';
 
-test('render', () => {
-  const { container } = render(<App />);
-  expect(container).toMatchSnapshot();
-});
+const initialState = {
+  phones: {
+    phoneList: []
+  }
+};
+
+const mockStore = configureStore([thunk]);
+
+describe('App', () => {
+  let store;
+  beforeEach(() => {
+    store = mockStore(initialState);
+  })
+
+  test('render', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+})
