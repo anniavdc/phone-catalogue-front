be3a84062d0b3a2ee9f7ba4ecb8e4b4bf0cfc908
annia <avdacosta@paradigmadigital.com>
2020-09-08 21:36:16 +0200

    Add not-found page logic and styles

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// files
+import searchIcon from './../assets/search-icon.png'
 
 export default function NotFound() {
     return (
-        <div>
-            NotFound
+        <div className="not-found-wrapper">
+            <div className="not-found-icon-wrapper">
+                <img src={searchIcon} alt="not found icon" className="search-icon" />
+                <p className="code">404</p>
+            </div>
+            <div className="not-found-content">
+                <p>Página no encontrada</p>
+                <p>Lo sentimos. No hemos podido encontrar la página que estás buscando</p>
+            </div>
+            <Link className="link" to="/app">
+                Ir a página principal
+            </Link>
         </div>
     )
 }
