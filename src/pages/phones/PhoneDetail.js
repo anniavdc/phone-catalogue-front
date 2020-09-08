7cac1d503ec7b0f05e6b7539997217caf8398734
annia <avdacosta@paradigmadigital.com>
2020-09-08 20:49:45 +0200

    Add loader and header title redirect to home

--- src/pages/phones/PhoneDetail.js
+++ src/pages/phones/PhoneDetail.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 // files
+import Loader from './../../components/loader/Loader';
 import { phoneList } from './../../store/actions/index';
 
 export default function PhoneDetail() {
@@ -21,25 +22,28 @@ export default function PhoneDetail() {
     const selectedPhone = phones.find(phone => phone.id === parseInt(phoneId));
     return (
         !selectedPhone
-            ? 'loading'
-            : <React.Fragment>
-                <h2 className="products-title">Phone Details</h2>
-                <div className="detail-wrapper">
-                    <div className="detail-image" style={{ backgroundImage: `url(${baseURL}/public/${selectedPhone.imageFileName})` }}></div>
-                    <div className="detail-content">
-                        <h3 className="detail-title">{selectedPhone.name}</h3>
-                        <p>{selectedPhone.manufacturer || ''}</p>
-                        <p>{selectedPhone.description || ''}</p>
-                        <div className="detail-features">
-                            <p>Features</p>
-                            <p>{selectedPhone.screen || ''}</p>
-                            <p>{selectedPhone.processor || ''}</p>
-                            <p>{selectedPhone.ram || ''}</p>
-                            <p><span className="detail-color" style={{ backgroundColor: selectedPhone.color || 'white' }}></span>{selectedPhone.color || ''}</p>
-                            <p className="detail-price">Price: {selectedPhone.price}</p>
+            ? (
+                <Loader extraClass="page" />
+            ) : (
+                <React.Fragment>
+                    <h2 className="products-title">Phone Details</h2>
+                    <div className="detail-wrapper">
+                        <div className="detail-image" style={{ backgroundImage: `url(${baseURL}/public/${selectedPhone.imageFileName})` }}></div>
+                        <div className="detail-content">
+                            <h3 className="detail-title">{selectedPhone.name}</h3>
+                            <p>{selectedPhone.manufacturer || ''}</p>
+                            <p>{selectedPhone.description || ''}</p>
+                            <div className="detail-features">
+                                <p>Features</p>
+                                <p>{selectedPhone.screen || ''}</p>
+                                <p>{selectedPhone.processor || ''}</p>
+                                <p>{selectedPhone.ram || ''}</p>
+                                <p><span className="detail-color" style={{ backgroundColor: selectedPhone.color || 'white' }}></span>{selectedPhone.color || ''}</p>
+                                <p className="detail-price">Price: {selectedPhone.price}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </React.Fragment>
+                </React.Fragment>
+            )
     )
 }
